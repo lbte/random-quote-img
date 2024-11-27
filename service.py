85d5ec4da4f82c6329bfa9eea9c5d187a0076ed0
lbte <lbustamante@unal.edu.co>
2024-11-27 10:59:33 -0500

    Update time

--- service.py
+++ service.py
@@ -46,8 +46,8 @@ def fetch_new_image():
 
 # Schedule the image fetching task
 scheduler = BackgroundScheduler()
-# Schedule the fetch to run daily at 10:10 AM
-scheduler.add_job(fetch_new_image, 'cron', hour=10, minute=50, second=0)
+# Schedule the fetch to run daily at 6:00 AM
+scheduler.add_job(fetch_new_image, 'cron', hour=6, minute=0, second=0)
 scheduler.start()
 
 @app.route('/')
